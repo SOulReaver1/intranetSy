dea1df644b566abe37fc27bc3f407d2605eee3e2
dev <dev@sc2consulting.fr>
2021-03-02 09:49:30 +0100

    :bug: Debug text sms auto"

--- src/Entity/SmsAuto.php
+++ src/Entity/SmsAuto.php
@@ -22,7 +22,7 @@ class SmsAuto
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      */
     private $content;
 
