70d1b9c30b67030af80918c1f43f776a3245a9fc
dev <dev@sc2consulting.fr>
2021-05-05 11:26:14 +0200

    :bug: Add commentary to search method

--- src/Controller/CustomerFilesController.php
+++ src/Controller/CustomerFilesController.php
@@ -77,6 +77,7 @@ class CustomerFilesController extends AbstractController
             ->add('home_phone', TextColumn::class, ['label' => 'Téléphone fixe'])
             ->add('mail_al', TextColumn::class, ['label' => 'Mail AL'])
             ->add('password_al', TextColumn::class, ['label' => 'Mot de passe AL'])
+            ->add('commentary', TextColumn::class, ['label' => 'Commantaire'])
             ->add('actions', TextColumn::class, [
                 'data' => function($context) {
                     return $context->getId();
