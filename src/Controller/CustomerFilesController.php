6e50942c17f5dccf8c8b1ce087178c8e2a374289
SOulReaver1 <57950180+SOulReaver1@users.noreply.github.com>
2021-09-24 10:26:05 +0200

    :bug: Debug in all customers

--- src/Controller/CustomerFilesController.php
+++ src/Controller/CustomerFilesController.php
@@ -222,9 +222,35 @@ class CustomerFilesController extends AbstractController
     /**
      * @Route("/all", name="customer_files_all", methods={"GET", "POST"})
      */
-    public function all(Request $request, GlobalStatut $global,DataTableFactory $dataTableFactory, CustomerFilesStatutRepository $customerFilesStatutRepository) {
+    public function all(Request $request, GlobalStatut $global,DataTableFactory $dataTableFactory, CustomerFilesStatutRepository $customerFilesStatutRepository, CustomerFilesRepository $customerFilesRepository) {
         $this->globalStatut = $global;
+        $editStatutForm = $this->createForm(StatusTransferType::class, [
+            "global" => $global
+        ]);
 
+        $editStatutForm->handleRequest($request);
+        if ($editStatutForm->isSubmitted() && $editStatutForm->isValid()) {
+            $data = $editStatutForm->getData();
+            $customer_files_ids = explode(', ', $data['customer_files']);
+            $statut = $data['customer_files_statut'];
+            $entityManager = $this->getDoctrine()->getManager();
+            if($data['customer_files']) {
+                foreach ($customer_files_ids as $value) {
+                    $customerFile = $customerFilesRepository->find(intval($value));
+                    // dd($value, $customerFile);
+                    $customerFile->setCustomerStatut($statut);
+                    $entityManager->persist($customerFile);
+                    $entityManager->flush();    
+                }
+                $this->addFlash('success', count($customer_files_ids).' fiche(s) on(t) bien été(s) modifiée(s) !');
+            }else {
+                $this->addFlash('error', 'Aucune fiche sélectionnée');
+            }
+
+            return $this->redirectToRoute('customer_files_index', [
+                'global' => $this->session->get('global')
+            ]);
+        }
         $table = $dataTableFactory->create()
             ->add('id', TextColumn::class, ['label' => '#'])
             ->add('global_statut', TextColumn::class, [
@@ -317,7 +343,8 @@ class CustomerFilesController extends AbstractController
 
         return $this->render('customer_files/index.html.twig', [
             'statuts' => $customerFilesStatutRepository->findAllByOrderWG(),
-            'datatable' => $table
+            'datatable' => $table,
+            'editStatutForm' => $editStatutForm->createView()
         ]);
     }
 
